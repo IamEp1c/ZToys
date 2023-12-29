5bf60efc6063a00e7f9f2a9a2821a5483ddf3dbc
IamEp1c <Qanjum96@gmail.com>
2023-12-29 16:03:25 -0500

    added angular frontend logic

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS `ZayanToyBox` (
+Id BIGINT NOT NULL PRIMARY KEY,
+name varchar(300),
+description varchar (2500)
+);
+
---
+++ src/test/java/com/example/ZayanToyBox/ZayanToyBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ZayanToyBox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZayanToyBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
